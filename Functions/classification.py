d1cedf23858ed575d8e494a99feda309c1b95306
Daniel Comaduran <dcomadur@ucalgary.ca>
2023-12-19 14:49:10 -0700

    Started implementing RG classifier. Not functional yet

--- Functions/classification.py
+++ Functions/classification.py
@@ -5,15 +5,31 @@
 """
 
 # Import libraries
-import sklearn
-import pyriemann
-import numpy as np
 import scipy
+import sklearn
+import numpy as np
+from pyriemann.estimation import Covariances
+from pyriemann.tangentspace import TangentSpace
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
 
 def ssvep_cca():
     """ Placeholder for CCA classifier to be used with SSVEP data. """
 
-def ssvep_riemann():
-    """ Placeholder based on Riemmanian geometry classifier
+def ssvep_riemann(epochs:np.ndarray, labels:np.ndarray):
+    """ 
+        SSVEP Riemmanian geometry classifier
+
+
         found here: https://moabb.neurotechx.com/docs/auto_examples/plot_cross_subject_ssvep.html#sphx-glr-auto-examples-plot-cross-subject-ssvep-py
     """
+
+    # Create pipeline
+    pipelines_fb = {}
+    pipelines_fb["RG+LogReg"] = make_pipeline(
+        Covariances(estimator="lwf"),
+        TangentSpace(),
+        LogisticRegression(solver="lbfgs", multi_class="auto"),
+        )
+    
+    
