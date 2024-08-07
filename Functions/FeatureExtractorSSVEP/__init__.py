d04b564cc47c7941a9087fe4e5122c39daa4ba01
Daniel Comaduran <dcomadur@ucalgary.ca>
2024-08-07 13:38:36 -0600

    Added NCAN classifier for comparison

---
+++ Functions/FeatureExtractorSSVEP/__init__.py
@@ -0,0 +1,5 @@
+from .featureExtractor import FeatureExtractor
+from .featureExtractorTemplateMatching import FeatureExtractorTemplateMatching
+from .featureExtractorCCA import FeatureExtractorCCA
+from .featureExtractorMEC import FeatureExtractorMEC
+from .featureExtractorMSI import FeatureExtractorMSI
